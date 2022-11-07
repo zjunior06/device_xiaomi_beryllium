e2743090fe0dabec667bf2f36c77ac1a0b40c0d9
PainKiller3 <ninadpatil100@gmail.com>
2022-11-07 09:17:35 +0530

    beryllium: Switch to RRO overlays
    Change-Id: Ibcd9f1f38b7eb0232f6a915db5b2ef00f4c7362e

--- device.mk
+++ device.mk
@@ -229,11 +229,16 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.2.vendor
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+    BerylliumCarrierConfigOverlay \
+    BerylliumFrameworksOverlay \
+    BerylliumNoCutoutOverlay \
+    BerylliumSettingsOverlay \
+    BerylliumSettingsProviderOverlay \
+    BerylliumSystemUIOverlay \
+    BerylliumTelephonyOverlay \
+    TetheringConfigOverlay \
+    WifiOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -357,10 +362,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
 
@@ -382,7 +383,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
-    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
